89c74dcc6195591e6f6e114f3ab9ac1a84928277
ngoncphucprodev <ngocphuc1399@gmail.com>
2023-04-10 13:26:25 +0700

    update fontEnd

--- src/components/ViewCart.jsx
+++ src/components/ViewCart.jsx
@@ -691,7 +691,7 @@ const ViewCart = () => {
                     sx={{ fontWeight: "bold" }}
                     color="text"
                   >
-                    {product.price} Đ
+                    {product.price} Đd
                     {/* {console.log(product.gallery, gall[index]._id.$oid)} */}
                   </Typography>
                   {/* {console.log(galleries.data[index]._id, product.gallery)} */}
